b7550af1bf6da52fc5c56dd248569efd1a32de39
MthsBttgr <mathiasbottger@gmail.com>
2022-03-31 13:16:25 +0200

    have put a bunch of stuff into arrays

--- sketch.js
+++ sketch.js
@@ -1,50 +1,61 @@
 const G = 1;
 
 let t = 0;
-let deltaTime = 1;
+let deltaTime = 0.7;
+
+let planets = [];
+let p = [];
+
+let mom = [];
 
 
 function setup() {
-  createCanvas(windowWidth-20, windowHeight-10);
+  createCanvas(windowWidth, windowHeight);
 
-  p1 = {x: 400, y: 400}
-  p2 = {x: 100, y: 400}
+  for (s = 0; s <= 9; s += 1)
+  {
+    p[s] = {
+    x: width / 2 - 120 * s, 
+    y: height / 2
+    }
 
+    mom[s] = {
+      x: 0,
+      y: 150 - 30 * s
+    }
+  }
 
-  let mom1 = {x: 0, y: 0}
-  let mom2 = {x: 0, y: 50}
+  mom[0].y = 0
 
-  sun = new Planet(10000, 70, p1, mom1, color(255, 214, 10))
-  planet = new Planet(10, 10, p2, mom2, color(10, 166, 75))
+  planets.push(new Planet(10000, 70, p[0], mom[0], color(255, 214, 10)))
+  planets.push(new Planet(10, 10, p[1], mom[1], color(random(255), random(255), random(255))))  
+  planets.push(new Planet(10, 10, p[2], mom[2], color(random(255), random(255), random(255))))
+  planets.push(new Planet(10, 10, p[3], mom[3], color(random(255), random(255), random(255))))
+  planets.push(new Planet(10, 10, p[4], mom[4], color(random(255), random(255), random(255))))
 
-  
+  console.log(planets)
+  console.log(p)
 
 }
 
 function draw() 
 {
   background(50);
-  
-  sun.force = gravity(p1, p2, sun.mass, planet.mass)
-  planet.force = gravity(p2, p1, planet.mass, sun.mass)
-
-  sun.momentum.x += sun.force.x * deltaTime
-  sun.momentum.y += sun.force.y * deltaTime
-  planet.momentum.x += planet.force.x * deltaTime
-  planet.momentum.y += planet.force.y * deltaTime
-
-  p1.x += sun.momentum.x / sun.mass * deltaTime
-  p1.y += sun.momentum.y / sun.mass * deltaTime
-
-  p2.x += planet.momentum.x / planet.mass * deltaTime
-  p2.y += planet.momentum.y / planet.mass * deltaTime
-
-  sun.showPlanet(p1)
-  planet.showPlanet(p2)
 
 
+  p[0].x += calculateplacement(p[0],p[1],0,1).x + calculateplacement(p[0],p[2],0,1).x
+  p[0].y += calculateplacement(p[0],p[1],0,1).y + calculateplacement(p[0],p[2],0,1).y
+  p[1].x += calculateplacement(p[1],p[0],1,0).x + calculateplacement(p[1],p[2],0,1).x
+  p[1].y += calculateplacement(p[1],p[0],1,0).y + calculateplacement(p[1],p[2],0,1).y
+  p[2].x += calculateplacement(p[2],p[0],2,0).x + calculateplacement(p[2],p[1],0,1).x
+  p[2].y += calculateplacement(p[2],p[0],2,0).y + calculateplacement(p[2],p[1],0,1).y
 
-  t += deltaTime;
+  for (s = 0; s <= 2; s++)
+  {
+    planets[s].showPlanet(p[s])
+    planets[s].showPlanet(p[s])
+    planets[s].showPlanet(p[s])
+  }
 }
 
 function gravity(p1, p2, mass1, mass2)
@@ -58,4 +69,22 @@ function gravity(p1, p2, mass1, mass2)
   let F = {x: Fg * rHat.x, y: Fg * rHat.y}
 
   return F
+}
+
+function calculateplacement(loc1, loc2, index1, index2)
+{
+  planets[index1].force = gravity(loc1, loc2, planets[index1].mass, planets[index2].mass)
+  
+  planets[index1].momentum.x += planets[index1].force.x * deltaTime
+  planets[index1].momentum.y += planets[index1].force.y * deltaTime
+
+  let x = planets[index1].momentum.x / planets[index1].mass * deltaTime
+  let y = planets[index1].momentum.y / planets[index1].mass * deltaTime
+
+  let dddd = {
+  x: x, 
+  y: y
+  }
+
+  return dddd
 }
